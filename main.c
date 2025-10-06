2e6d138f0411e1238541349010718f554e1bf0fe
yazn_108 <102805404+yazn-108@users.noreply.github.com>
2025-10-06 12:11:49 +0300

    auto-c-watcher

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-  printf("Follow me on social media (Beni sosyal medyada takip et): ");
-  printf("@yazn_108");
-  printf("\n\nMy personal website (Kişisel sitem): ");
-  printf("https://yazn-108.github.io/");
+  printf("Follow me on social media (Beni sosyal medyada takip et):");
+  printf("\033[1;34m @yazn_108 \033[0m");
+  printf("\n\nMy personal website (Kişisel sitem):");
+  printf("\033[1;34m https://yazn-108.github.io/ \033[0m");
   return 0;
 }
